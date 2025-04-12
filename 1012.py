81152c36af34ff82f1421516b3d11d9f6c32f8db
micael <00001155042244sp@al.educacao.sp.gov.br>
2025-04-11 21:25:36 -0300

    Create 1012.py

---
+++ 1012.py
@@ -0,0 +1,11 @@
+a,b,c = map(float,input().split())
+r=(a*c)/2
+r2=3.14159 * c**2
+r3=((a+b) *c)/2
+r4=b**2
+r5=a*b
+print('TRIANGULO: %.3f' %r)
+print('CIRCULO: %.3f' %r2)
+print('TRAPEZIO: %.3f' %r3)
+print('QUADRADO: %.3f' %r4)
+print('RETANGULO: %.3f' %r5)
